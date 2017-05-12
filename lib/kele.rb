3932dac9bb4a14ef4a7bb7647f6b8e2c25c68027
Andrew <andrewgovroom@hotmail.com>
2017-05-12 21:30:17 +0000

    Mentor Availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,21 +5,27 @@ require 'json'
 
 class Kele
   include HTTParty
-   base_uri "https://www.bloc.io/api/v1/"
+
   
  
 
     def initialize(email, password)
-        response = self.class.post(base_api_endpoint("sessions"), body:{"email": email, "password": password})
+        response = self.class.post(api_url("sessions"), body:{"email": email, "password": password})
         raise InvalidStudentCodeError.new() if response.code == 401
         @auth_token = response["auth_token"]
     end
 
     def get_me
       response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
-       @user_data = JSON.parse(response.body)
+        @user_data = JSON.parse(response.body)
     
     end
+    
+    def get_mentor_availability(mentor_id)
+      response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+      @mentor_availability = JSON.parse(response.body)
+     end
+    
 
 
 private
